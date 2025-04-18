9b02496216cb2d036be00a1c5312dfc3fd3c449b
rayan_aouf <rayanaouf1512@gmail.com>
2025-04-18 14:55:47 +0100

    request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller("auth")
 export class AuthController{
     constructor(private readonly authService: AuthService) {}
+
+    @Post("/signup")
+    signup(@Body() dto:any){
+        console.log({
+            dto : dto
+        })
+        return this.authService.signup()
+    }
+
+    @Post("/login")
+    login(){
+        return this.authService.login()
+    }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { Bookmark   } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Injectable({})
 export class AuthService{
-
-
+    constructor(private prisma : PrismaService){}
+    signup(){
+        return {msg : "i have signed in"}
+    }
+    login(){
+        return {msg : "i have logged in"}
+    }
 }
