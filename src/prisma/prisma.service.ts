3735af47ac65452b392ad511c0e5bf0c07e04212
rayan_aouf <rayanaouf1512@gmail.com>
2025-04-18 22:07:32 +0100

    istall and use the config nest js module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(){
+    constructor(config : ConfigService){
         super({
             datasources : {
                 db : {
-                    url : process.env.DATABASE_URL
+                    url : config.get('DATABASE_URL')
                 }
             }
         })
+
     }
 }
