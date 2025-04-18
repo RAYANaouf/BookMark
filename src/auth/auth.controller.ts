4baeded4532ac2d441239cd938850dc78eb9005b
rayan_aouf <rayanaouf1512@gmail.com>
2025-04-18 17:49:54 +0100

    request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
 
 @Controller("auth")
 export class AuthController{
     constructor(private readonly authService: AuthService) {}
 
     @Post("/signup")
-    signup(@Body() dto:any){
+    signup(@Body() authDto:AuthDto){
         console.log({
-            dto : dto
+            dto : authDto
         })
         return this.authService.signup()
     }
