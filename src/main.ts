b98fb11f27ad962f4511231077ff9683bda43bd4
rayan_aouf <rayanaouf1512@gmail.com>
2025-05-20 19:44:59 +0100

    introduce firebase progressing...

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,11 @@ async function bootstrap() {
   }))
 
 
+  app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+  });
+
 
   //firebase admin
 
