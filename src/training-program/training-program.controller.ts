891a9caf8b91d5acdd755a0a0201e5d4c41df61a
rayan_aouf <rayanaouf1512@gmail.com>
2025-06-06 08:12:06 +0100

    progressing....

--- src/training-program/training-program.controller.ts
+++ src/training-program/training-program.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { TrainingProgramService } from './training-program.service';
 import { CreateTrainingProgramDto } from './dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Controller('training-program')
 export class TrainingProgramController {
 
     constructor(private readonly trainingProgramService: TrainingProgramService) {}
 
+    
+    @HttpCode(HttpStatus.CREATED)
     @Post("create")
-    async create(@Body() dto: CreateTrainingProgramDto) {
+    @UseInterceptors(
+      FileInterceptor("cover",{
+        storage : memoryStorage(), //in-memory buffer
+        limits : {fileSize: 5 * 1024 * 1024 } //5MB limit
+      })
+    )
+    async create(
+      @Body() dto: CreateTrainingProgramDto,
+      @UploadedFile() file? 
+  ) {
       console.log("create training program ====>> " , dto)
       return this.trainingProgramService.create(dto);
     }
