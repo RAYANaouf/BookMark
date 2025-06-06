82a54238121f7322ea8c457908fb4682f9bd133d
rayan_aouf <rayanaouf1512@gmail.com>
2025-06-06 07:52:25 +0100

    progressing....

--- src/training-program/dto/training-program.dto.ts
+++ src/training-program/dto/training-program.dto.ts
@@ -1,36 +1,28 @@
 // src/modules/training-program/dto/create-training-program.dto.ts
 
 import { Type } from 'class-transformer';
-import { IsString, IsOptional, IsNumber, IsNumberString } from 'class-validator';
+import { IsString, IsOptional, IsNumber, IsNumberString, IsNotEmpty } from 'class-validator';
 
 export class CreateTrainingProgramDto {
+  
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
+  @IsNotEmpty()
   description: string;
 
-  @IsOptional()
-  @IsString()
   forWho?: string;
 
-  @IsOptional()
-  @IsString()
   prerequisites?: string;
 
-  @IsOptional()
-  @IsString()
   whatYouWillLearn?: string;
 
-  @IsOptional()
-  @IsString()
   whatYouCanDoAfter?: string;
 
-  @IsOptional()
-  @IsNumberString()
   minAge : number
 
-  @IsOptional()
-  @IsNumberString()
   maxAge : number
+
 }
