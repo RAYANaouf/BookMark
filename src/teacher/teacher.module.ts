d0e9e15eaa3ef4d55e6edcdb07df3aa33088c8bc
rayan_aouf <rayanaouf1512@gmail.com>
2025-06-13 19:15:48 +0100

    progressing ...

--- src/teacher/teacher.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TeacherService } from './teacher.service';
-import { TeacherController } from './teacher.controller';
-
-@Module({
-  providers: [TeacherService],
-  controllers: [TeacherController]
-})
-export class TeacherModule {}
