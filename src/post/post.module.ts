b0ef76377ef3f0d6f1dc8a99b5c8c6b6afb8c961
rayanaouf <rayanaouf1512@gmail.com>
2025-06-22 13:19:03 +0200

    fix error server

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 
 @Module({
-  providers: [PostService]
+  providers: [PostService],
+  exports : [PostService]
 })
 export class PostModule {}
