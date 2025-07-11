c77b440064383f001f9101aa280f067fec2975a5
rayanaouf <rayanaouf1512@gmail.com>
2025-07-11 19:13:06 +0200

    debug commit

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -19,7 +19,7 @@ export class CourseController {
   @Get('all')
   getAllCourses(@Request() req){
     const userId = getUserIdFromRequest(req)
-    console.log("start testtt ====>> " , req)
+    console.log("start testtt ====>> " , req.user)
     console.log("userId ====>> " , userId)
     if(!userId){
       return this.courseService.getAll()
