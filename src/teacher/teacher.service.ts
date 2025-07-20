6b4622a6e7c4ba262fbe9052afefbeb2bc26a446
rayanaouf <rayanaouf1512@gmail.com>
2025-07-20 23:34:20 +0200

    add teacher end points

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TeacherService {}
+export class TeacherService {
+    constructor(private prisma: PrismaService) {}
+
+    async getAllTeachers() {
+        return this.prisma.user.findMany({
+            where: {
+                academyLinks: {
+                    some: {
+                        role: 'Teacher'
+                    }
+                }
+            },
+            select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                profilePhoto: true,
+                academyLinks: {
+                    where: { role: 'Teacher' },
+                    select: {
+                        academy: {
+                            select: {
+                                id: true,
+                                name: true,
+                                logo: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    async getTeachersByAcademy(academyId: number) {
+        return this.prisma.user.findMany({
+            where: {
+                academyLinks: {
+                    some: {
+                        academyId,
+                        role: 'Teacher'
+                    }
+                }
+            },
+            select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                profilePhoto: true,
+                academyLinks: {
+                    where: { 
+                        academyId,
+                        role: 'Teacher' 
+                    },
+                    select: {
+                        role: true,
+                        academy: {
+                            select: {
+                                id: true,
+                                name: true,
+                                logo: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
